91c5b62808811816bfb605337b7eb2658926869f
PerikiyoXD <perikiyoxd@gmail.com>
2023-03-27 23:03:25 +0200

    Update isoabduct.py
    AI upgraded

--- isoabduct.py
+++ isoabduct.py
@@ -1,77 +1,75 @@
 import argparse
 import requests
 import base64
+import sys
+import os
 
-def makeRequestWithUseragent(url, useragent):
-    request_headers = { "User-Agent" : useragent }
-    response = None
+
+def make_request_with_user_agent(url, user_agent):
+    headers = {"User-Agent": user_agent}
     try:
-        response = requests.get(url, headers=request_headers)
-    except requests.exceptions.Timeout:
-        print("/!\\ Got a timeout. Probably the host is down. Check yourself!")
-        sys.exit()
-    except requests.exceptions.TooManyRedirects:
-        print("/!\\ Too many redirects. Probably the host is avoiding you! Check user agent!")
-        sys.exit()
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
     except requests.exceptions.RequestException as e:
-        print("/!\\ There was an ambiguous exception that occurred while handling your request")
-        sys.exit()
-    except requests.ConnectionError:
-        print("/!\\ There was a connection error!")
+        print(f"/!\\ Error: {e}")
         sys.exit()
     return response
 
-def debase64(data):
-    return base64.b64decode(data)
-    
-def download_iso(url):
+
+def download_iso(url, output_dir, user_agent):
     print(f"Downloading ISO from {url}...")
-    resp = makeRequestWithUseragent(url, args.u)
+    response = make_request_with_user_agent(url, user_agent)
     print("Downloaded")
-    file_name = url.rsplit('/', 1)[-1]
-    print(f"Saving as {file_name}...")
-    out_file = f"{args.o}{file_name}"
-    with open(out_file, "w") as f:
-            f.write(resp.text)
+    file_name = os.path.basename(url)
+    out_file = os.path.join(output_dir, file_name)
+    print(f"Saving as {out_file}...")
+    with open(out_file, "wb") as f:
+        f.write(response.content)
     print("Saved to disk")
     return out_file
 
-if __name__ == "__main__":
+
+def debase64(data):
+    return base64.b64decode(data)
+
+
+def main():
     print("""
           @
     ISO (/^\)BDUCT!
         /(o)\ <[Gather and decode]
        /_____\\""")
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("URL", help="URL to download and decode")
-    parser.add_argument("-o", help="Output directory", default=".")
-    parser.add_argument("-i", help="Use local fake iso")
-    parser.add_argument("-u", help="Custom user agent", default="Mozilla/5.0 (Windows NT 10.0; rv:40.0) Gecko/20100101 Firefox/40.0")
-    
+    parser.add_argument("url", help="URL to download and decode")
+    parser.add_argument("-o", "--output-dir", help="Output directory", default=".")
+    parser.add_argument("-i", "--input-file", help="Use local fake iso")
+    parser.add_argument("-u", "--user-agent", help="Custom user agent", default="Mozilla/5.0 (Windows NT 10.0; rv:40.0) Gecko/20100101 Firefox/40.0")
     args = parser.parse_args()
-            
-    print(f"Output: {args.o}")
-    
+
+    print(f"Output: {args.output_dir}")
+
     # Gather response from the URL
-    if args.i:
-        iso_file = args.i
-        print(f"Using {args.i} (Local file), skipping download!")
+    if args.input_file:
+        iso_file = args.input_file
+        print(f"Using {args.input_file} (Local file), skipping download!")
     else:
-        iso_file = download_iso(args.URL)
-           
-    with open(iso_file, "r") as f:
+        iso_file = download_iso(args.url, args.output_dir, args.user_agent)
+
+    with open(iso_file, "rb") as f:
         data = f.read()
-    
+
     print("First Base64 decode...")
-    decoded = base64.b64decode(data)
-    with open(f"{args.o}encoded.b64", "wb") as f:
-        f.write(decoded)  
-    print("OK")       
+    decoded = debase64(data)
+    with open(os.path.join(args.output_dir, "encoded.b64"), "wb") as f:
+        f.write(decoded)
+    print("OK")
     print("Second Base64 decode...")
-    decoded = base64.b64decode(decoded)
-    with open(f"{args.o}decoded.zip", "wb") as f:
-        f.write(decoded) 
-    print("OK") 
-    print("Decoded zip recovered succesfully")
-        
+    decoded = debase64(decoded)
+    with open(os.path.join(args.output_dir, "decoded.zip"), "wb") as f:
+        f.write(decoded)
+    print("OK")
+    print("Decoded zip recovered successfully")
+
+
+if __name__ == "__main__":
+    main()
